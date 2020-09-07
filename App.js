94cf33d15d4bbe9fed92706663e6846fe098fa83
rodrigobertolotti <rodrigobertolotti>
2020-09-07 17:49:41 -0300

    Primer commit

--- App.js
+++ App.js
@@ -1,21 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { NativeRouter, Route, Link, Switch } from "react-router-native";
+import Bag from './Components/Bag/Bag';
+import Calendar from './Components/Calendar/Calendar';
+import Trip from './Components/Trip/Trip';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends React.Component {
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+      <NativeRouter>
+        <View style={styles.body}>
+          <Switch>
+            <Route path="/trip" component={Calendar} />
+            <Route path="/bag" component={Calendar} />
+            <Route path="/calendar" component={Calendar} />
+            <Route component={Calendar} />
+          </Switch>
+        </View>
+        <View style={styles.footer}>
+          <View style={styles.footerBox}>
+            <Link to="/trip" component={TouchableOpacity}>
+              <View style={styles.center}>
+                <Icon name="plane" size={24} color="grey" />
+              </View>
+            </Link>
+          </View>
+          <View style={styles.footerBox}>
+            <Link to="/bag" component={TouchableOpacity}>
+              <View style={styles.centrado}>
+                <Icon name="suitcase" size={24} color="grey" />
+              </View>
+            </Link>
+          </View>
+          <View style={styles.footerBox}>
+            <Link to="/calendar" component={TouchableOpacity}>
+              <View style={styles.centrado}>
+                <Icon name="calendar" size={24} color="grey" />
+              </View>
+            </Link>
+          </View>
+        </View>
+      </NativeRouter>
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    },
+  body: {
+    flex: 12,
+    backgroundColor: 'rgba(78,188,255,0.5)',
+    opacity: 1
   },
-});
+  center: {
+    alignItems: 'center'
+  },
+  footer: {
+    display: 'flex',
+    flexDirection: 'row',
+    backgroundColor: 'rgba(255,255,255,0.8)',
+    height: 60,
+    flex: 1
+  },
+  footerBox: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  footerText: {
+    fontSize: 10,
+    color: 'white',
+    fontWeight: 'bold',
+    marginTop: 5
+  }
+
+})
+
+export default App;
---
+++ Components/Calendar/Calendar.js
@@ -0,0 +1,158 @@
+import React from 'react';
+import { View, Text, ImageBackground, StyleSheet, Button, TouchableOpacity, Alert } from 'react-native';
+
+class Calendar extends React.Component {
+
+    state = {
+    }
+
+    componentDidMount(){
+        this.setState({
+            manana: true,
+            imagen: 'https://wallpaperaccess.com/full/1260643.jpg',
+            descripcion: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+        })
+    }
+    cambiarMomento = () => {
+        let img = "";
+        let descripcion = " ";
+        let actual = this.state.manana;
+        if (!this.state.manana){
+            img = 'https://wallpaperaccess.com/full/1260643.jpg';
+            descripcion= 'HLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.';
+        }else{
+            img = 'https://www.mordeo.org/files/uploads/2018/04/Empire-State-Building-New-York-City-HD-Mobile-Wallpaper.jpg';
+            descripcion= 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.';
+        }
+        this.setState({
+            imagen: img,
+            manana: !actual,
+            descripcion: descripcion
+        })
+        
+    }
+
+    render() {
+
+        return (
+            <Momento
+                dia={this.props.dia}
+                descripcion={this.state.descripcion}
+                imagen={this.state.imagen}
+                onClick={() => this.cambiarMomento()}
+                manana={this.state.manana}
+            ></Momento>
+        )
+    }
+}
+
+export default Calendar;
+
+function Momento(props) {
+    const image = { uri: props.imagen };
+    return (
+        <ImageBackground source={image} style={styles.fondo}>
+            <View style={styles.column}>
+                <View style={styles.row}>
+
+                </View>
+                <View style={styles.row}>
+                    <View style={styles.boxTitulo}>
+                        <Text style={styles.titulo}>
+                            {props.dia}
+                        </Text>
+                    </View>
+                    <View style={styles.boxDescripcion}>
+                        <Text style={styles.descripcion}>
+                            {props.descripcion}
+                        </Text>
+                    </View>
+                    <View style={styles.boxBotones}>
+                        <View style={styles.rowBoton}></View>
+                        <View style={styles.rowBoton}>
+                            <View style={styles.containerBoton}>
+                                <TouchableOpacity
+                                    disabled={props.manana}
+                                    onPress={props.onClick}
+                                    style={styles.boton}>
+                                    <Text style={styles.textoBoton}>
+                                        MAÑANA
+                                    </Text>
+                                </TouchableOpacity>
+                            </View>
+                            <View style={styles.containerBoton}>
+                                <TouchableOpacity
+                                    disabled={!props.manana}
+                                    onPress={props.onClick}
+                                    style={styles.boton}>
+                                    <Text style={styles.textoBoton}>
+                                        TARDE
+                                    </Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+            </View>
+        </ImageBackground>
+    )
+}
+
+const styles = StyleSheet.create({
+    fondo: {
+        height: '100%',
+        width: '100%'
+    },
+    column: {
+        display: 'flex',
+        flex: 1
+    },
+    row: {
+        flexDirection: 'column',
+        flex: 1,
+    },
+    titulo: {
+        fontSize: 30,
+        color: 'white',
+        marginLeft: 20,
+    },
+    descripcion: {
+        fontSize: 16,
+        color: 'white',
+        margin: 10
+    },
+    boxTitulo: {
+        flex: 1,
+    },
+    boxBotones: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    boxDescripcion: {
+        flex: 3
+    },
+    boton: {
+        borderRadius: 5,
+        backgroundColor: 'white',
+        width: '80%',
+        height: '70%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        opacity: 0.7
+    },
+    textoBoton: {
+        fontSize: 12,
+        color: 'black'
+    },
+    rowBoton: {
+        flex: 1,
+        flexDirection: 'row'
+    },
+    containerBoton: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
